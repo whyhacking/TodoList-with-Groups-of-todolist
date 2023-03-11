f8b0256ad39961fdfe44e79b48990ade3cd17a3f
Akash Gupta <2135462@swansea.ac.uk>
2023-03-11 01:17:13 +0000

    added default in todo migration to complete field

--- database/migrations/2023_03_10_212612_create_todos_table.php
+++ database/migrations/2023_03_10_212612_create_todos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->boolean('completed');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
