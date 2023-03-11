6286a36aaaeace9b573537cd97f71bd603594f6f
Akash Gupta <2135462@swansea.ac.uk>
2023-03-11 01:17:54 +0000

    updated controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    //
+    
+    public function index(){
+        return "hii";
+    }
+
+    public function create(){
+        return view('todolist.create');
+    }
+
+    public function update(Request $request){
+
+         $todo = $request->title;
+         Todo::create(['title' => $todo ]);
+    }
+    public function edit(){
+        return "hii";
+    }
+
+
+
 }
