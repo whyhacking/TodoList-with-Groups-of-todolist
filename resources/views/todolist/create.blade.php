0a12a64da966db2c2bd9ecb3d9470485882c6b4c
Akash Gupta <2135462@swansea.ac.uk>
2023-03-11 01:17:42 +0000

    Updated create php

---
+++ resources/views/todolist/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo List | Create</title>
+</head>
+<body style="text-align:center">
+    
+    <h1>Create ToDo List</h1>
+    <form action="/update" method="post">
+        @csrf
+        <input type="text" name="title" />
+        <input type="submit" value="Create" />
+
+    </form>
+    <br>
+    <a href="/index">BACK</a>
+</body>
+</html>
